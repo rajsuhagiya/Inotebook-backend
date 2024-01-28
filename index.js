3c26fec11db1b05bd70d158e4250156cc86edbb1
raj suhagiya <https.helloworld@gmail.com>
2024-01-28 17:14:52 -0500

    added models and routes

--- index.js
+++ index.js
@@ -6,10 +6,9 @@ connectToMongo();
 const app = express();
 const port = 4000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-  console.log("hello");
-});
+//Available Routes
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
